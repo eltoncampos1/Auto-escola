a9f802fd4a806674fee3115d612cb19e49f92972
eltoncampos1 <qazwsx272518>
2020-11-19 02:39:14 -0300

    fix font-family

--- autoescola/src/styles/GlobalStyle.js
+++ autoescola/src/styles/GlobalStyle.js
@@ -1,4 +1,7 @@
+import React from "react";
 import { createGlobalStyle } from "styled-components";
+import { Helmet } from "react-helmet";
+
 import { BreakPointSizes, breakAt } from "./BreakPoints";
 
 const GlobalStyle = createGlobalStyle` 
@@ -81,4 +84,16 @@ const GlobalStyle = createGlobalStyle`
     }
 `;
 
-export default GlobalStyle;
+const GlobalStyleComposed = () => (
+  <>
+    <GlobalStyle />
+    <Helmet>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600;700&display=swap"
+        rel="stylesheet"
+      />
+    </Helmet>
+  </>
+);
+
+export default GlobalStyleComposed;
